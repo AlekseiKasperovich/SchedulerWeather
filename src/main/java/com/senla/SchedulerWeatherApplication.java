1c0831fd8bd8f89110a57d5f77d61350685804d8
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-11-09 15:57:24 +0300

    first commit

---
+++ src/main/java/com/senla/SchedulerWeatherApplication.java
@@ -0,0 +1,12 @@
+package com.senla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerWeatherApplication.class, args);
+    }
+}
